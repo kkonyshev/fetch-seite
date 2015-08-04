ce1c487685680fee3f3fe8ba78144ea581d54e45
kkonyshev <hs@ngs.ru>
2015-08-04 20:43:08 +0600

    2

--- build.sbt
+++ build.sbt
@@ -2,9 +2,11 @@ name := "fetch-site"
 
 version := "1.0"
 
-scalaVersion := "2.11.0"
+scalaVersion := "2.11.6"
+
+resolvers += "spray repo" at "http://repo.spray.io"
 
 libraryDependencies += "org.scala-lang" % "scala-xml" % "2.11.0-M4"
 libraryDependencies += "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2.1"
-
-
+libraryDependencies += "io.spray" %% "spray-client" % "1.3.3"
+libraryDependencies += "com.typesafe.akka" % "akka-actor_2.11" % "2.3.9"
--- src/main/scala-2.11/FetchSite.scala
+++ src/main/scala-2.11/FetchSite.scala
@@ -1,8 +1,14 @@
-import java.net.{InetSocketAddress, Proxy, URL}
 
+import java.net.InetSocketAddress
+
+import akka.actor.ActorSystem
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
+import spray.http._
+import spray.client.pipelining._
 
-import scala.io.Source
+import scala.concurrent.Future
+import scala.io.{Codec, Source}
+import scala.util.{Failure, Success}
 import scala.xml.factory.XMLLoader
 import scala.xml.{Elem, NodeSeq, XML, Node}
 /**
@@ -10,12 +16,6 @@ import scala.xml.{Elem, NodeSeq, XML, Node}
  * Created by konishev on 18/06/2015.
  */
 object FetchSite {
-
-  def proc(node: Node): String =
-    node match {
-      case <body>{ txt }</body> => "Partial content: " + txt
-      case _ => "grmpf"
-    }
 
   def main2 (args: Array[String]) {
     val price = "447 552.85 RUB"
@@ -26,18 +26,24 @@ object FetchSite {
     println(priceWrapped)
   }
 
+  def main1(args: Array[String]) {
+    //val site = fetchCommonSite("https://www.fabrikant.ru/trades/atom/PriceMonitoring/?action=view&id=2831")
+    //println(site)
+  }
+
   def main (args: Array[String]) {
 
-    for (i<-1.to(3))
+    for (i<-1.to(2))
     {
-      process(fetchSite(i))
+      fetchSite(i, process)
     }
   }
 
-  val saxParcer: XMLLoader[Elem] = XML.withSAXParser((new SAXFactoryImpl).newSAXParser)
 
-  def process(site: String): Unit = {
-    println(site)
+
+  def process(site: String) = {
+    //println(site)
+    val saxParcer: XMLLoader[Elem] = XML.withSAXParser((new SAXFactoryImpl).newSAXParser)
     val content =  saxParcer.loadString(site)
     println("total count: " + getCount(content))
 
@@ -46,10 +52,12 @@ object FetchSite {
       if (divClassAttribute.isDefined) {
         if (divClassAttribute.get.toString().equalsIgnoreCase("Search-result-item")) {
           val wrapper: NodeSeq = div \\ "div" //Search-wrapper
-          println("NODE_dump: " + wrapper(0).child(1))
+          //println("NODE_dump: " + wrapper(0).child(1))
           for (n <- wrapper.iterator) {
-            if (n.attribute("class").isDefined) {
-              if (n.attribute("class").get.toString().equalsIgnoreCase("Search-item-1")) {
+            val classAttribute: Option[Seq[Node]] = n.attribute("class")
+            if (classAttribute.isDefined) {
+              val classAttributeValue: String = classAttribute.get.toString()
+              if (classAttributeValue.equalsIgnoreCase("Search-item-1")) {
                 println("---base-info---")
                 val root: NodeSeq = n \\ "div" //Search-item-1
                 val content: NodeSeq = root \\ "div" //Search-item-content
@@ -64,7 +72,7 @@ object FetchSite {
                 println("price: " + priceWrapped)
                 println("type: " + (option.last.child(1) \\ "ul" \\ "li" \\ "span").text.trim)
               }
-              else if (n.attribute("class").get.toString().equalsIgnoreCase("Search-item-2")) {
+              else if (classAttributeValue.equalsIgnoreCase("Search-item-2")) {
                 println("---org-info---")
                 val root: NodeSeq = n \\ "div" //Search-item-2
                 val content: NodeSeq = root \\ "div" //Search-item-content
@@ -73,7 +81,7 @@ object FetchSite {
                 println("ogr name: " + (option(1).child(3).child(3)).text.trim)
                 println("ogr link: " + (option(1).child(3) \\ "a" \\ "@href").text.trim)
               }
-              else if (n.attribute("class").get.toString().equalsIgnoreCase("Search-item-3")) {
+              else if (classAttributeValue.equalsIgnoreCase("Search-item-3")) {
                 println("---status-info---")
                 val root: NodeSeq = n \\ "div" //Search-item-2
                 val content: NodeSeq = root \\ "div" //Search-item-content
@@ -83,7 +91,7 @@ object FetchSite {
                 println("publish date: " + option(3).child(2).text.trim)
                 println("end date: " + option(4).child(2).text.trim)
               }
-              else if (n.attribute("class").get.toString().equalsIgnoreCase("Search-item-4")) {
+              else if (classAttributeValue.equalsIgnoreCase("Search-item-4")) {
                 println("---traders-info---")
                 val root: NodeSeq = n \\ "div" //Search-item-2
                 val content: NodeSeq = root \\ "div" //Search-item-content
@@ -112,14 +120,24 @@ object FetchSite {
     return 0
   }
 
-  val proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ftc.ru", 3128))
+  //val proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("proxy.ftc.ru", 3128))
 
-  def fetchSite(pageNumber: Int=1) = {
-    val url = new URL("https://www.fabrikant.ru/trades/procedure/search/?page="+pageNumber)
-    val connection = url.openConnection()//url.openConnection(proxy)
-    val stream = Source.fromInputStream(connection.getInputStream)
-    val page = stream.getLines().map(_.toString).mkString
-    stream.close()
-    page
+  def fetchSite(pageNumber: Int=1, func: (String) => Unit) = {
+    fetchCommonSite("https://www.fabrikant.ru/trades/procedure/search/?page="+pageNumber, func)
+  }
+
+  def fetchCommonSite(targetUrl: String, func: (String) => Unit) = {
+    implicit val system = ActorSystem()
+    import system.dispatcher // execution context for futures
+
+    val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
+
+    val response: Future[HttpResponse] = pipeline(Get(targetUrl))
+
+    val r = response onComplete{
+      case Failure(ex) => ex.printStackTrace()
+      case Success(resp) => println("success for: " + targetUrl)
+        func.apply(resp.message.entity.asString)
+    }
   }
 }
